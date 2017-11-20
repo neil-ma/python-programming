a0c5701b0e119ef1b99151d0444622b3d4ddd169
jay <maninglwj@163.com>
2017-11-21 01:16:41 +0800

    sys

--- system-module/os_module.py
+++ system-module/os_module.py
@@ -11,3 +11,11 @@
 
 @desc: 
 '''
+import os
+
+# pathsep：目录列表中分割目录的符号；sep：目录组分隔符；pardir：父目录；curdir：当前目录；linesep：行分隔符，在windows中是回车换行\r\n。
+print(os.pathsep,os.sep,os.pardir,os.curdir,os.linesep)
+
+#os.path提供了一整套目录处理的工具。包括os.path.isfile,os.path.isdir,os.path.exists,os,path.getsize
+print(os.path.isfile(r'C:\Users'),os.path.isdir(r'C:\Users'),os.path.exists(r'C:\Users'),os.path.getsize(r'C:\Users') )
+
