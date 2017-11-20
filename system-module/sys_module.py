dd92e6bc4ff4e8cf597c4bf8cb795e46e4155a7a
natty.ma <natty.ma@vipshop.com>
2017-11-20 16:03:12 +0800

    sys module

--- system-module/sys_module.py
+++ system-module/sys_module.py
@@ -14,11 +14,40 @@
        MODULE REFERENCE
                 https://docs.python.org/3.6/library/sys
 
+       (1) sys.path: 是一个目录的列表，指定模块的搜索路径。
+       (2) sys.modules：是一个字典。
+
 
 '''
+import sys,traceback
+#介绍关于sys.path变量
+def sys_path():
+    print(sys.path)
+    #sys.path初始化自环境变量PYTHONPATH，并且可以修改。但是对sys.path的修改不是永久的，在python进程关闭后失效。
+    sys.path.append(r"D:\My_Own\music")
+    print(sys.path)
 
-import sys
+def sys_module():
+    # Python会话或者程序导入的每一个模块，在sys.modules字典中都有一个 name:module项
+    # 例如：'os': <module'os'from'D: \\Programes\\python-3.5.2\\lib\\os.py'>
+    print(sys.modules)
+
+def sys_exception():
+    #sys模块中的一些属性，为我们提供最近抛出的python异常的相关信息。
+    #sys.exc_info()会返回一个元组，包含异常的类型、值和追踪对象。 traceback模块
+    try:
+        raise TypeError("value define error!!")
+    except:
+        exc_info = sys.exc_info()
+        print(exc_info)
+        print(exc_info[0])
+        print(exc_info[1])
+        print(traceback.print_tb(exc_info[2]))
+
 
 if __name__ == '__main__':
     # str_method_test()
-    print(sys.platform,sys.maxsize,sys.version)
+    # print(sys.platform,sys.maxsize,sys.version)
+    # sys_path()
+    # sys_module()
+    sys_exception()
