2f3bb48d56150bf8753747105ce73d6626fa0f30
natty.ma <natty.ma@vipshop.com>
2017-12-13 19:48:24 +0800

    file_operation

---
+++ file-directory-tools/dir_tools.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+# encoding: utf-8
+'''
+
+@author: natty
+
+@file: dir_tools.py
+
+@time: 2017/12/13 19:07
+
+@desc: 目录工具的使用：
+        (1)遍历目录：
+'''
+
+def traverse_dir():
+    print()
